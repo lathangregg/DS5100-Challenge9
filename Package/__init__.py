d9422b332716892cf625e28e672f598d5612999d
lathangregg <lwgregg5@gmail.com>
2024-07-02 11:07:49 -0400

    did stuff

--- Package/__init__.py
+++ Package/__init__.py
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 print("Welcome to my module!")
+=======
+from setuptools import setup
+
+setup(
+	name = 'Test'
+	version = '0.1'
+	description = 'A demo package'
+	author = 'Lathan'
+	license = 'MIT'
+	packages = ['Package']
+)
+>>>>>>> 6f4759e (setup)
